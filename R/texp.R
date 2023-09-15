89b910933e962a5199874ec4d5cb3d11b2cf4732
Robert <rpchastain@protonmail.com>
2023-09-15 04:20:51 -0700

    Added initial package build

--- R/texp.R
+++ R/texp.R
@@ -1,7 +1,7 @@
 #' Inverse CDF of the truncated exponential distribution intended to be used 
 #' for sampling
 #'
-#' @param x Input value
+#' @param x Input value, expected to be between 0 and 1
 #' @param rate Single value specifying the rate.
 #' @param a Lower truncation bound, if not provided it assumes the default value
 #' of 0.
@@ -11,6 +11,8 @@
 #' @export 
 #'
 #' @examples
+#' inv_texp(0.25, b = 6)
+#' inv_texp(0.5, rate = -1, a = 2, b = 6)
 inv_texp <- function(x, rate = 1, a = 0, b) {
   log(exp(-rate * a) - x * (exp(-rate * a ) - exp(-rate * b)))/(-rate)
 }
@@ -28,7 +30,9 @@ inv_texp <- function(x, rate = 1, a = 0, b) {
 #' @export
 #'
 #' @examples
+#' rtexp(5, b = 6)
+#' rtexp(2, rate = -1, a = 2, b = 6)
 rtexp <- function(n, rate = 1, a = 0, b) {
-  u <- runif(n)
+  u <- stats::runif(n)
   inv_texp(u, rate, a, b)
 }
